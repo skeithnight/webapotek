cf7bc0e13d1af69a69af89a276e42c1b5e0bae2a
dwiki nugraha <dwiki.nugraha@2359media.com>
2019-10-09 10:42:22 +0700

    commit

--- src/views/DataTransaksi/DataTransaksi.js
+++ src/views/DataTransaksi/DataTransaksi.js
@@ -1,45 +1,177 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import classNames from 'classnames';
-import { Button, Row, Col } from 'reactstrap'
-import { rgbToHex } from '@coreui/coreui/dist/js/coreui-utilities'
+import {
+  Button, Row, Col, Modal, ModalBody, ModalFooter, ModalHeader, Form,
+  FormGroup,
+  Input,
+  Label,
+} from 'reactstrap'
+import { rgbToHex } from '@coreui/coreui/dist/js/coreui-utilities';
+import axios from 'axios';
+import { baseUrl } from "../../data.js";
 
 // Import React Table
 import { MDBDataTable, MDBBtn } from 'mdbreact';
+// First way to import
+import { ClipLoader } from 'react-spinners';
 
 class DataObat extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      datas: [],
+      datatransaksi: [],
+      isLoading: false,
+      error: null,
+      tambah: false,
+      hapus: false,
+      name: '',
+      apotek: '',
+      jk: '',
+      ttl: '',
+      noTelp: '',
+      email: '',
+      password: '',
+      latitude: '',
+      longitude: '',
+      isLoadingAddForm: false,
+      isLoadingRemoveForm: false,
+      idHapus: ''
+    };
+
+    this.toggleHapus = this.toggleHapus.bind(this);
+  }
+
+  toggleHapus(id) {
+    this.setState({
+      hapus: !this.state.hapus,
+      idHapus: id
+    });
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+
+    // obat
+    var apiBaseUrl = baseUrl + "/transaksi";
+    var self = this;
+
+    axios({
+      url: apiBaseUrl,
+      method: 'get',
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('token')
+      }
+    }).then(response =>
+      response.data.map(data => ({
+        id: `${data.id}`,
+        namaObat: data.produk !=null ? `${data.produk[0].produk.nama}` : '',
+        harga: data.produk !=null ? `${data.produk[0].produk.harga}` : '',
+        apotek: data.apotek != null ? `${data.apotek.name}` : '',
+        jumlah: data.produk !=null ? `${data.produk[0].jumlah}` : '',
+        status: `${data.status}`,
+        opsi: [<Button color="danger" onClick={() => this.toggleHapus(data.id)} className="mr-1"><i className="fa fa-trash mr-2 blue-text" aria-hidden="true"></i></Button>]
+      }))
+    )
+      // Let's make sure to change the loading state to display the data
+      .then(datas => {
+        console.log(datas);
+        this.setState({
+          datas,
+          isLoading: false
+        });
+      })
+      // We can still use the `.catch()` method since axios is promise-based
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  handleRemove(){
+
+    this.setState({ isLoadingRemoveForm: true });
+
+    var apiBaseUrl = baseUrl + "/transaksi/"+this.state.idHapus;
+    var self = this;
+
+    console.log(apiBaseUrl);
+    axios({
+      url: apiBaseUrl,
+      method: 'delete',
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('token')
+      },
+    })
+      .then(response => { window.location.href = '/'; }
+      )
+      // We can still use the `.catch()` method since axios is promise-based
+      .catch(error => this.setState({ error, isLoadingRemoveForm: false }));
+  }
+
   render() {
+    const { datas, isLoading, error, isLoadingRemoveForm } = this.state;
+    let inputBtn;
+    let removeBtn;
+
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+
+    if (isLoading) {
+      return <p>Loading ...</p>;
+    }
+
+    if (isLoadingRemoveForm) {
+      removeBtn = <ClipLoader sizeUnit={"px"} color={'#123abc'} loading={this.state.loading} />;
+    } else {
+      removeBtn = <Button color="danger" onClick={this.handleRemove.bind(this)}>Ya</Button>;
+    }
+
     const data = {
       columns: [
+        // {
+        //   label: 'No',
+        //   field: 'name',
+        //   sort: 'asc',
+        //   width: 150
+        // },
         {
-          label: 'Id_obt',
-          field: 'name',
-          sort: 'asc',
-          width: 150
-        },
-        {
-          label: 'Nama Obat',
-          field: 'position',
+          label: 'id_tsk',
+          field: 'id',
           sort: 'asc',
           width: 270
         },
         {
-          label: 'Jumlah',
-          field: 'office',
+          label: 'Nama Obat',
+          field: 'namaObat',
           sort: 'asc',
           width: 200
         },
         {
           label: 'Harga',
-          field: 'age',
+          field: 'harga',
           sort: 'asc',
           width: 100
         },
         {
-          label: 'Kadaluarsa',
-          field: 'date',
+          label: 'Apotek',
+          field: 'apotek',
           sort: 'asc',
-          width: 150
+          width: 200
+        },
+        {
+          label: 'Jumlah',
+          field: 'jumlah',
+          sort: 'asc',
+          width: 100
+        },
+
+        {
+          label: 'Status',
+          field: 'status',
+          sort: 'asc',
+          width: 100
         },
         {
           label: 'Opsi',
@@ -47,24 +179,8 @@ class DataObat extends Component {
           width: 100
         }
       ],
-      rows: [
-        {
-          name: 'Tiger Nixon',
-          position: 'System Architect',
-          office: 'Edinburgh',
-          age: '61',
-          date: '2011/04/25',
-          opsi: [<Button color="danger"><i className="fa fa-trash mr-2 blue-text" aria-hidden="true"></i></Button>]
-        },
-        {
-          name: 'Garrett Winters',
-          position: 'Accountant',
-          office: 'Tokyo',
-          age: '63',
-          date: '2011/07/25',
-          opsi: [<Button color="danger"><i className="fa fa-trash mr-2 blue-text" aria-hidden="true"></i></Button>]
-        }
-      ]
+
+      rows: this.state.datas
     };
     return (
       <div className="animated fadeIn">
@@ -78,13 +194,25 @@ class DataObat extends Component {
                 </Button>
               </Col>
             </Row> */}
+            <Modal isOpen={this.state.hapus} toggle={this.toggleHapus}
+              className={'modal-danger ' + this.props.className}>
+              <ModalHeader toggle={this.toggleHapus}>Hapus Data</ModalHeader>
+              <ModalBody>
+                Apakah yakin akan menghapus data ini?
+                  </ModalBody>
+              <ModalFooter>
+                {removeBtn}
+                <Button color="secondary" onClick={() => { this.toggleHapus(this.state.idHapus) }}>Tidak</Button>
+              </ModalFooter>
+            </Modal>
+
             <br />
-            {/* <MDBDataTable
+            <MDBDataTable
               striped
               bordered
               small
               data={data}
-            /> */}
+            />
           </div>
         </div>
       </div>
